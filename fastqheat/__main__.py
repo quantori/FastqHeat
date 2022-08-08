e80a32e06129550447e8648878aff5bde3626351
dmitriy.grossman <dmitriy.grossman@quantori.com>
2022-08-08 13:34:02 +0300

    adds clarification comment

--- fastqheat/__main__.py
+++ fastqheat/__main__.py
@@ -53,7 +53,7 @@ def _make_accession_list(terms: tp.Iterable[str]) -> list[str]:
     """Get an accession list based on pattern of the given term."""
     accession_list = list()
     for term in terms:
-        if not term:
+        if not term:  # validate_accession_file may return some empty strings. Here is the easiest place to deal with it
             continue
         if SRR_PATTERN.search(term):
             accession_list.append(term)
